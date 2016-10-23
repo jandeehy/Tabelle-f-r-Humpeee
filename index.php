fb14e3df0408b0aacaedd27d87879de1bc2b8809
Jan <fog.borchersboy@gmail.com>
2016-10-23 19:36:38 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Fritz - Humpeee</title>
+        <link rel="stylesheet" href="./Fritz - Humpeee_files/style.css" type="text/css">
+    </head>
+    <body>
+        <header> 
+
+            <center><font size="25">Blackwater - RPG</font></center>
+
+
+            <ul>
+                <nav>
+
+                    <font size="5">
+
+                    <li>Home</a></li>
+                    <li>Regeln</a></li>
+                    <li>Sonstiges</a></li>
+
+                    </font>
+
+                </nav>
+
+            </ul>
+
+
+            <center>============================================================</center>
+
+
+        </header>
+        <br>
+        <div class="datagrid"><table>
+                <thead><tr><th>Regel</th><th>Strafe</th><th>Bemerkung</th></tr></thead>
+                <tbody><tr><td>Nicht schreien</td><td>Schreien wird bestraft mit einem Bann von 24h!</td><td>---</td></tr>
+                    <tr class="alt"><td>Nicht beleidigen</td><td>Beleidigen wird bestraft mit einem Bann!</td><td>---</td></tr>
+                    <tr><td>Immer auf die Team-Mitglieder hören</td><td>Den Forderungen der Team-Mitglieder nicht nachgehen wird bestraft mit einem Kick oder einem Bann von 24h!</td><td>data</td></tr>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
