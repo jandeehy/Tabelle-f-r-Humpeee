c3e7e5666a15f3c362ed1fcff3c57beec0412672
Jan <fog.borchersboy@gmail.com>
2016-10-23 19:37:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,18 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!-- saved from url=(0053)https://blubberberndlp.github.io/AltisRPG/regeln.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Fritz - Humpeee</title>
         <link rel="stylesheet" href="./Fritz - Humpeee_files/style.css" type="text/css">
     </head>
+
+
     <body>
+
         <header> 
 
             <center><font size="25">Blackwater - RPG</font></center>
 
 
-            <ul>
-                <nav>
-
-                    <font size="5">
-
-                    <li>Home</a></li>
-                    <li>Regeln</a></li>
-                    <li>Sonstiges</a></li>
-
-                    </font>
-
-                </nav>
-
-            </ul>
 
 
             <center>============================================================</center>
@@ -39,11 +22,57 @@ and open the template in the editor.
         <br>
         <div class="datagrid"><table>
                 <thead><tr><th>Regel</th><th>Strafe</th><th>Bemerkung</th></tr></thead>
-                <tbody><tr><td>Nicht schreien</td><td>Schreien wird bestraft mit einem Bann von 24h!</td><td>---</td></tr>
-                    <tr class="alt"><td>Nicht beleidigen</td><td>Beleidigen wird bestraft mit einem Bann!</td><td>---</td></tr>
-                    <tr><td>Immer auf die Team-Mitglieder hören</td><td>Den Forderungen der Team-Mitglieder nicht nachgehen wird bestraft mit einem Kick oder einem Bann von 24h!</td><td>data</td></tr>
+                <tbody><tr><td>Nicht schreien</td><td>Schreien wird bestraft mit einem Bann von 24h!</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Nicht beleidigen</td><td>Beleidigen wird bestraft mit einem Bann!</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
+                    <tr class="alt"><td>Weitere Regel</td><td>Weitere Strafe</td><td>Bemerkung</td></tr>
                 </tbody>
             </table>
         </div>
-    </body>
-</html>
+            <!--        <table align="center">
+                        <caption>+Regeln+</caption>
+                        <tr>
+                            <td>Regel</td>
+                            <td>Strafe</td>
+                            <td>Bemerkung</td>
+                        </tr>
+                        <br>
+                        <tr>
+                            <td>Nicht beleidigen</td><td>Beleidigen wird bestraft mit einem Bann!</td><td>---</td>
+                        </tr>
+                        <br>
+                        <tr>
+                            <td>Nicht schreien</td><td>Schreien wird bestraft mit einem Bann von 24h!</td><td>---</td>
+                        </tr>
+                        <br>
+                        <tr>
+                            <td>Immer auf die Team-Mitglieder hören</td><td>Den Forderungen der Team-Mitglieder nicht nachgehen wird bestraft mit einem Kick oder einem Bann von 24h!</td><td>---</td>
+                        </tr>
+                    </table>-->
+
+        <footer>
+        </footer>
+
+
+
+
+
+
+
+    </body></html>
